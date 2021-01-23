6ce4e610d7bf2c20af2093d0fcec9ee347252188
Motolola <motolola@icloud.com>
2021-01-23 13:20:26 +0000

    May second commit.

---
+++ src/test/java/io/motolola/certification/CertificationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.motolola.certification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
