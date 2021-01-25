f7149b7445b27953941f87287f4591fafbc113f0
Motolola <motolola@icloud.com>
2021-01-25 13:18:00 +0000

    Testing literals.

---
+++ src/main/java/io/motolola/certification/primitive/Test.java
@@ -0,0 +1,15 @@
+package io.motolola.certification.primitive;
+
+public class Test {
+
+    public static void main(String[] args) {
+        int myInt = 1_234_567;
+        System.out.println(myInt);
+
+        int myBin = 0B1_1;
+        System.out.println(myBin);
+
+        int myHex = 0xe;
+        System.out.println(myHex);
+    }
+}
